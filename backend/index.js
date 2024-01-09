c332d9fb46c03ebab0dffa8ce755a254bf8547c9
Ion Basha <ionbasha15@gmail.com>
2024-01-08 21:40:11 -0500

    Started frontend dev

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+
+const { MongoClient } = require('mongodb')
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/submissions', async (req, res) => {
+    const client = new MongoClient(process.env.MONGO_URI)
+
+    try {
+        await client.connect()
+        const database = client.db('ideaboxdb')
+        const submissionCollection = database.collection('submissions')
+
+        const submissionData = await submissionCollection.find({}).toArray()
+        
+        res.status(200).json(submissionData)
+    }
+    catch(e) {
+        res.json({ error : e})
+    }
+    finally {
+        await client.close()
+        console.log("Closing MongoDB client...")
+    }
+})
+
+app.listen(process.env.PORT, () => { console.log(`Listening on PORT ${process.env.PORT} `)})
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='displayedContent'>
+      <div className='topPage'>
+        <h1>TheIdeaBox</h1>
+        <p>Spread your ideas here!</p>
+      </div>
+      
+
+    </div>
+  )
+}
+
+export default Home
+
