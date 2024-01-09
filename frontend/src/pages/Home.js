7a5156c25fb464a7778ae3801d78ffd1ebd9f9b5
Ion Basha <ionbasha15@gmail.com>
2024-01-08 22:00:11 -0500

    Added page title on frontend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Home = () => {
+  
+  useEffect(() => {
+    console.log("Hello")
+  })
+
   return (
     <div className='displayedContent'>
       <div className='topPage'>
         <h1>TheIdeaBox</h1>
-        <p>Spread your ideas here!</p>
+        <p>Put your coding project ideas here!</p>
       </div>
-      
-
     </div>
   )
 }
