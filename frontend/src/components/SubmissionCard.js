f741c7d01b399e7224423565ed949ce08da44860
Ion Basha <ionbasha15@gmail.com>
2024-01-11 01:12:02 -0500

    Added like button functionality

--- frontend/src/components/SubmissionCard.js
+++ frontend/src/components/SubmissionCard.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import { useState } from 'react'
 
 const SubmissionCard = ({ submission }) => {
+
+  const [likes, setLikes] = useState(0)
+
+  const handleClick = () => {
+    setLikes(likes+1)
+    console.log(likes)
+  }
+
   return (
     <div className='subCard'>
+      <div className='buttonBar'>
+        <button className='buttonLike' onClick={handleClick}></button>
+        <label>{likes}</label>
+      </div>
       <label>Project Title:</label>
       <p>{submission.title}</p>
       <label>Description:</label>
